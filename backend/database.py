7d63166a9d6ea7c90cd122d6c75a7cbbf9a9fee2
aadit <adimit136@gmail.com>
2026-09-13 21:47:59 +0530

    update : made database persistent

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,14 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./crm.db"
+# If DATABASE_URL exists in the environment, use it. Otherwise, use local crm.db.
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./crm.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
